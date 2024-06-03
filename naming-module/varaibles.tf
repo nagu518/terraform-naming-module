aa263d20f28ae987def03173a10a958bf5d24962
NagarjunaReddy Bonam <new@mail.com>
2024-06-02 21:34:01 -0500

    new updates

--- naming-module/varaibles.tf
+++ naming-module/varaibles.tf
@@ -1,10 +1,10 @@
 variable "base_name" {
-  description = "The base name for the resource."
+  description = "The base name for the resource"
   type        = string
 }
 
 variable "resource_type" {
-  description = "The type of the resource."
+  description = "The type of the resource"
   type        = string
   validation {
     condition     = contains(["virtual_machine", "key_vault", "storage_account"], var.resource_type)
