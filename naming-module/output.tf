44b07fdb240f2850540e873f9735a4be74773940
NagarjunaReddy Bonam <new@mail.com>
2024-06-02 21:37:30 -0500

    new updates

--- naming-module/output.tf
+++ naming-module/output.tf
@@ -1,5 +1,9 @@
 output "resource_name" {
-  description = "The generated resource name."
-  value       = chomp(file("${path.module}/generated_name.txt"))
+  description = "The generated resource name"
+  value       = coalesce(
+    try(null_resource.resource_name[0].triggers.resource_name, null),
+    try(null_resource.key_vault_name[0].triggers.resource_name, null),
+    try(null_resource.storage_account_name[0].triggers.resource_name, null)
+  )
 }
 
